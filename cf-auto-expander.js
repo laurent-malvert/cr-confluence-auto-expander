790ae8797738b7f46b6d9306db21725febb9f9d7
Laurent Malvert <laurent.malvert@gmail.com>
2016-05-04 09:47:17 +0200

    fixed invalid non-confluence shortcut safeguard

--- cf-auto-expander.js
+++ cf-auto-expander.js
@@ -1,17 +1,17 @@
 
-if (document.querySelectorAll('#com-atlassian-confluence')) {
+if (document.querySelector('#com-atlassian-confluence')) {
 
   console.log(" -- cf-auto-expander");
-  
+
   /* jquery-based auto-expander */
-  
+
   // $('.expand-control > .icon:not(.expanded)')
   //   .parent()
   //   .click();
-  
-  
+
+
   /* pure-js auto-expander      */
-  
+
   []
     .slice.call(document.querySelectorAll('.expand-control > .icon:not(.expanded)'))
     .map(function (e) { return (e.parentNode); })
